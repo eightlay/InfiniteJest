54281c98f5f5d15a4ec8c9665adad2344642d50f
Timur Dzhalalov <timurdzhalalov@yahoo.com>
2021-02-26 18:18:54 +0300

    Add project files.

---
+++ InfiniteJest/InfiniteJest.cpp
@@ -0,0 +1,12 @@
+﻿// InfiniteJest.cpp : Defines the entry point for the application.
+//
+
+#include "InfiniteJest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
